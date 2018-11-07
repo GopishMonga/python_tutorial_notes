3dd59472f93927f08cb00073b1614caefbbbb78b
Gopish Monga <gopishmonga@gmail.com>
2018-11-07 18:31:56 +0530

    Accessing Dictionary

--- src/dictionary.py
+++ src/dictionary.py
@@ -10,3 +10,13 @@ def print_dictionary(dictionary):
 # Initialize Dictionary
 man = {"firstName":"Gopish","lastName":"Monga","age":24}
 print_dictionary(man)
+
+# Accessing Values
+
+## Using [key]  ===> only use when key exists, otherwise error is thrown
+firstName = man["firstName"]
+print("firstName = " + firstName)
+
+## Using get method ===> returns None if key not found, also default value can be given
+height = man.get("height","5 feet 8 inches")
+print("height = " + height)
