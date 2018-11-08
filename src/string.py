e8f77222a3de58c0e13e4f032a08231726199be5
Gopish Monga <gopishmonga@gmail.com>
2018-11-08 14:17:59 +0530

    string

---
+++ src/string.py
@@ -0,0 +1,18 @@
+# Strings
+## Strings are arrays of bytes representing Unicode characters.
+## However, Python does not have a character data type,
+## a single character is simply a string with a length of 1.
+
+# Initialize String
+## Strings can be initialized with single,double and triple quotes
+string1 = 'Hello World!'
+string2 = "Let's try 'single quotes'"
+string3 = '''Let's try "double quotes" '''
+string4 = '''This string is
+                    a multiple line
+            string. '''
+
+print(string1)
+print(string2)
+print(string3)
+print(string4)
