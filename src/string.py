c16c243ff33cccfce895b396694ad8d119a0df43
Gopish Monga <gopishmonga@gmail.com>
2018-11-08 15:02:36 +0530

    accessing characters in strings

--- src/string.py
+++ src/string.py
@@ -16,3 +16,21 @@ print(string1)
 print(string2)
 print(string3)
 print(string4)
+
+# Accessing characters
+## individual characters accessed by Indexing
+## Negative index can be used to access from end of string
+## While accessing an index out of the range will cause an IndexError
+## Only Integers are allowed to be passed as an index, float or,
+## other types will cause a TypeError
+
+name = "Gopish Monga"
+first_char = name[0]
+last_char = name[-1]
+print(name)
+print(first_char)
+print(last_char)
+
+## Slicing can be used to access substrings
+print(name[3:6])
+print(name[3:-2])
