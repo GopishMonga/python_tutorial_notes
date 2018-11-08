0b6664271ebbc3a775a6b07f160c816eba332ecb
Gopish Monga <gopishmonga@gmail.com>
2018-11-08 16:38:51 +0530

    Formatting of Strings, Numbers, Float

--- src/string.py
+++ src/string.py
@@ -74,3 +74,39 @@ print(String1)
 String1 = r"This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
 print("\nPrinting Raw String in HEX Format: ")
 print(String1)
+
+# Formatting of Strings, Numbers, Float
+## Default order
+String1 = "{} {} {}".format('Geeks', 'For', 'Life')
+print("\nPrint String in default order: ")
+print(String1)
+
+## Positional Formatting
+String1 = "{1} {0} {2}".format('Geeks', 'For', 'Life')
+print("\nPrint String in Positional order: ")
+print(String1)
+
+## Keyword Formatting
+String1 = "{l} {f} {g}".format(g = 'Geeks', f = 'For', l = 'Life')
+print("\nPrint String in order of Keywords: ")
+print(String1)
+
+## Formatting of Integers
+String1 = "{0:b}".format(16)
+print("\nBinary representation of 16 is ")
+print(String1)
+
+## Formatting of Floats
+String1 = "{0:e}".format(165.6458)
+print("\nExponent representation of 165.6458 is ")
+print(String1)
+
+## Rounding off Integers
+String1 = "{0:.2f}".format(1/6)
+print("\none-sixth is : ")
+print(String1)
+
+## String alignment
+String1 = "|{:<10}|{:^10}|{:>10}|".format('Geeks','for','Geeks')
+print("\nLeft, center and right alignment with Formatting: ")
+print(String1)
