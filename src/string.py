91fea5f432df13bf2bc31af94e1337afdfaf2d81
Gopish Monga <gopishmonga@gmail.com>
2018-11-08 16:26:48 +0530

    Escape Sequence

--- src/string.py
+++ src/string.py
@@ -42,4 +42,35 @@ print(name)
 
 ## Deleting Strings
 del name
-### print(name) will through an error "name is not defined" 
+### print(name) will through an error "name is not defined"
+
+# Escape Sequence
+## Initial String
+String1 = '''I'm a "Geek"'''
+print("Initial String with use of Triple Quotes: ")
+print(String1)
+
+## Escaping Single Quote
+String1 = 'I\'m a "Geek"'
+print("\nEscaping Single Quote: ")
+print(String1)
+
+## Escaping Doule Quotes
+String1 = "I'm a \"Geek\""
+print("\nEscaping Double Quotes: ")
+print(String1)
+
+## Printing Paths with the use of Escape Sequences
+String1 = "C:\\Python\\Geeks\\"
+print("\nEscaping Backslashes: ")
+print(String1)
+
+## Printing Geeks in HEX
+String1 = "This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print("\nPrinting in HEX with the use of Escape Sequences: ")
+print(String1)
+
+## Using raw String to ignore Escape Sequences
+String1 = r"This is \x47\x65\x65\x6b\x73 in \x48\x45\x58"
+print("\nPrinting Raw String in HEX Format: ")
+print(String1)
