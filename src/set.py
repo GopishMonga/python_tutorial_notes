44633d7546b3e11b6b641446e8a7f77806fb8451
Gopish Monga <gopishmonga@gmail.com>
2018-11-09 22:03:38 +0530

    Set initialization

---
+++ src/set.py
@@ -0,0 +1,22 @@
+# Set is an unordered collection of data type that is iterable,
+# mutable and has no duplicate elements
+
+# Initialization
+set1 = set()
+print('\nInitialized empty set')
+print(set1)
+
+# Initialization with String
+set1 = set("Helloo Worrrlddd!!!!")
+print('\nSet initialized with string')
+print(set1)
+
+# Initialization with list
+set1 = set([1,1,1,2,2,2,3,3,3,4,4,4,5,5,5])
+print('\nSet initialized with list')
+print(set1)
+
+# Initialization with mixed data types
+set1 = set(["hello",1,1,2,3,"world"])
+print('\nSet initialized with list')
+print(set1)
