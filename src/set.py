39084eba11a962a3de8bc4f4be4ad97f3867c5e1
Gopish Monga <gopishmonga@gmail.com>
2018-11-09 22:28:44 +0530

    Inserting elements to set

--- src/set.py
+++ src/set.py
@@ -20,3 +20,26 @@ print(set1)
 set1 = set(["hello",1,1,2,3,"world"])
 print('\nSet initialized with list')
 print(set1)
+
+# Adding element to the Set
+set1.add(8)
+set1.add(9)
+set1.add(12)
+print("\nSet after Addition of Three elements: ")
+print(set1)
+
+# Adding elements to the Set using Iterator
+for i in range(1, 6):
+    set1.add(i)
+print("\nSet after Addition of elements from 1-5: ")
+print(set1)
+
+# Adding Tuples to the Set
+set1.add((6,7))
+print("\nSet after Addition of a Tuple: ")
+print(set1)
+
+# Addition of elements to the Set using Update function
+set1.update([10, 11])
+print("\nSet after Addition of elements using Update: ")
+print(set1)
