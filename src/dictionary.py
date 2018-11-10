e5f10d44943f3badaf753e837745d61b2fc3fde3
Gopish Monga <gopishmonga@gmail.com>
2018-11-10 19:05:04 +0530

    adding elements to dictionary

--- src/dictionary.py
+++ src/dictionary.py
@@ -63,3 +63,30 @@ print("\nheight = " + height)
 print("\nAdding height")
 man["height"] = "5 feet 8 inches"
 print_dictionary(man)
+
+# Creating an empty Dictionary
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+
+# Adding elements one at a time
+Dict[0] = 'Geeks'
+Dict[2] = 'For'
+Dict[3] = 1
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+
+# Adding set of values to a single Key
+Dict['Value_set'] = 2, 3, 4
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+
+# Updating existing Key's Value
+Dict[2] = 'Welcome'
+print("\nUpdated key value: ")
+print(Dict)
+
+# Adding Nested Key value to Dictionary
+Dict[5] = {'Nested' :{'1' : 'Life', '2' : 'Geeks'}}
+print("\nAdding a Nested Key: ")
+print(Dict)
